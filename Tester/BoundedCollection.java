4ab263735d7d3cb9c8d1a98e19717297bb31e997
Ju young Jeon <juyoungjeon@Juui-MacBook-Pro.local>
2018-07-08 00:36:40 +0900

    source code

---
+++ Tester/BoundedCollection.java
@@ -0,0 +1,20 @@
+package Tester;
+import java.util.Collection;
+
+public interface BoundedCollection<E> extends Collection<E> {
+
+    /**
+     * Returns true if this collection is full and no new elements can be added.
+     *
+     * @return <code>true</code> if the collection is full
+     */
+    boolean isFull();
+
+    /**
+     * Gets the maximum size of the collection (the bound).
+     *
+     * @return the maximum number of elements the collection can hold
+     */
+    int maxSize();
+
+}
